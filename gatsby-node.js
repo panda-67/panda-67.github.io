6f7f0f5307010958cb0974e20e41e8632d2e0924
Lord Arba <samsul.m@mhs.unsyiah.ac.id>
2022-10-12 19:55:16 +0700

    ok

--- gatsby-node.js
+++ gatsby-node.js
@@ -42,7 +42,7 @@ exports.createPages = ({ graphql, actions }) => {
     Array.from({ length: numPages }).forEach((_, i) => {
       createPage({
         path: i === 0 ? `/blog` : `/blog/${i + 1}`,
-        component: path.resolve("./src/pages/blog.js"),
+        component: path.resolve("./src/templates/blog-list.js"),
         context: {
           limit: postsPerPage,
           skip: i * postsPerPage,
