61ff66198d2d60afacf738e5af30466e9a6f4eec
Samsul Muarrif <samuarrif@gmail.com>
2024-10-31 22:13:13 +0700

    update article list on entomologist

--- src/pages/entomologist.js
+++ src/pages/entomologist.js
@@ -1,13 +1,29 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import Frame from '../layouts/main'
-import { useSiteMetadata } from "../hooks/use-site-metadata"
+import * as React from "react";
+import { Link } from "gatsby";
+import Frame from "../layouts/main";
+import { useSiteMetadata } from "../hooks/use-site-metadata";
 
-export default function EntomologistPage({ pageContext: { breadcrumb: { crumbs } } }) {
+export default function EntomologistPage({
+  pageContext: {
+    breadcrumb: { crumbs },
+  },
+}) {
   return (
     <Frame bread={crumbs}>
       <article className="w-full max-w-xs md:max-w-2xl lg:max-w-6xl mx-auto px-4 my-4 space-y-8">
         <ul className="flex flex-col gap-y-6 py-4 text-base">
+          <Link to="https://smujo.id/biodiv/article/view/15093">
+            <li>
+              <h4 className="font-semibold text-current md:text-lg hover:text-sky-700">
+                Description of the termite genus Prohamitermes (Termitinae) with
+                new characters from the Leuser Ecosystem, Sumatra, Indonesia
+              </h4>
+              <p>Biodiversitas Journal of Biological Diversity (2023)</p>
+              <author className="text-sm">
+                Samsul Muarrif, Samadi, Jauharlina, Syaukani
+              </author>
+            </li>
+          </Link>
           <Link to="https://www.hindawi.com/journals/tswj/2022/9475722/">
             <li>
               <h4 className="font-semibold text-current md:text-lg hover:text-sky-700">
@@ -71,16 +87,20 @@ export default function EntomologistPage({ pageContext: { breadcrumb: { crumbs }
         </ul>
       </article>
     </Frame>
-  )
+  );
 }
 
 export function Head() {
-  const { meta } = useSiteMetadata()
+  const { meta } = useSiteMetadata();
   return (
     <>
       <title>Entomologist | {meta.title}</title>
       <meta name="keywords" content="resume, portfolio, profile" />
-      <meta name="description" content="The list of my publication with all collegue." />
+      <meta
+        name="description"
+        content="The list of my publication with all collegue."
+      />
     </>
-  )
+  );
 }
+
