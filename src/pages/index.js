e27d1c2205d708fd81e56cde5dc0517ac5b3d758
Samsul Muarrif <samuarrif@gmail.com>
2025-04-12 17:41:32 +0700

    update 12/04/25

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,11 @@ import { useSiteMetadata } from "../hooks/use-site-metadata";
 export default function IndexPage({ data, path }) {
   return (
     <Frame path={path}>
-      <div className="w-full h-full flex flex-wrap -mt-8 md:-mt-16">
+      <div className="w-full h-full flex flex-wrap flex-row -mt-8 md:-mt-16">
         {/* Expertise */}
-        <section className="w-full">
+        <section className="w-1/4">
           <Header>Expertise</Header>
-          <div className="mx-6 grid gap-4 sm:grid-cols-3">
+          <div className="mx-6 flex flex-wrap flex-col space-y-4">
             <CardLink
               href="https://panda-67.github.io/programming"
               src="https://cdn.pixabay.com/photo/2016/11/23/14/45/coding-1853305__340.jpg"
@@ -30,18 +30,18 @@ export default function IndexPage({ data, path }) {
             {/* <CardLink href='#' src='https://cdn.pixabay.com/photo/2018/03/24/08/56/colorful-3256055__340.jpg' alt='Graphic Design'> */}
             {/*   Graphic Design */}
             {/* </CardLink> */}
-            <CardLink
-              href="https://rampago.github.io"
-              src="https://cdn.pixabay.com/photo/2020/05/06/12/01/compass-5137269__340.jpg"
-              alt="Mapping Expert"
-            >
-              Mapping Expert
-            </CardLink>
+            {/* <CardLink */}
+            {/*   href="https://rampago.github.io" */}
+            {/*   src="https://cdn.pixabay.com/photo/2020/05/06/12/01/compass-5137269__340.jpg" */}
+            {/*   alt="Mapping Expert" */}
+            {/* > */}
+            {/*   Mapping Expert */}
+            {/* </CardLink> */}
           </div>
         </section>
 
         {/* Latest Blog */}
-        <section className="w-full">
+        <section className="w-3/4">
           <Header>Latest Post</Header>
           <div className="mx-6 py-2 min-h-max rounded-lg border border-indigo-600 bg-gradient-to-tr from-base-300 to-transparent text-slate-800">
             {data.allMdx.edges
@@ -77,7 +77,7 @@ export function Head() {
 
 export const indexQuery = graphql`
   {
-    allMdx(sort: { frontmatter: { date: DESC } }, limit: 5) {
+    allMdx(sort: { frontmatter: { date: DESC } }, limit: 7) {
       edges {
         node {
           frontmatter {
